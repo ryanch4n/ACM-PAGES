cf7dc8944cf782987ffe03250ecc5ffb61335b6c
Ryan Chan <ryanch4n@gmail.com>
2017-08-27 21:16:52 -0500

    Added preliminary files

---
+++ config.py
@@ -0,0 +1,17 @@
+class Config:
+    pass
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+class TestingConfig(Config):
+    TESTING = True
+
+class ProductionConfig(Config):
+    pass
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+}
